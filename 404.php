10d82cba1f157a8d1f25c501e07088e34207252f
tsubaki <124051426+tsutsubaki@users.noreply.github.com>
2023-12-07 22:08:47 +0900

    23tscommit

--- 404.php
+++ 404.php
@@ -9,12 +9,20 @@
 
 <?php get_header(); ?> <!-- header.php読み込み -->
 
+<div class="l-main">
+    <div class="l-main__content">
 <div class="container">
+<div class="p-contents p-single__contents">
     <h1>404 Not Found</h1>
     <p>ページが見つかりませんでした</p>
     <a href="<?= get_site_url(); ?>">トップページへ戻る</a>
     <!-- <p>Topページは<a href="<?php echo esc_url(home_url('/')); ?>">こちら</a></p> -->
 
 </div>
+</div>
+</div>
+
+</div>
 <?php get_footer(); ?>
 <?php get_sidebar(); ?>
+</article><!--wrapper-->
